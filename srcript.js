c2a2e027af0a4a262157d922caedb02895f11790
Treasuremathew1 <168614400+Treasuremathew1@users.noreply.github.com>
2024-12-08 18:53:12 +0100

    Add files via upload

---
+++ srcript.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal(".home-text", {
+    ...scrollRevealOption,
+    origin: "left",
+  });
+
+  const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
